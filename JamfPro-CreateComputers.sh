0e6e72d5f251c3ee0d7af37f3f25580c445ce3e1
Luis Giraldo <luis@ook.co>
2016-10-20 11:48:47 -0500

    break curl commands into separate lines for readability

--- JamfPro-CreateComputers.sh
+++ JamfPro-CreateComputers.sh
@@ -34,7 +34,10 @@ while read col1 col2
 do
 	counter=$[$counter+1]
 	apiData="<computer><general><name>${col1}</name><serial_number>${col2}</serial_number><platform>Mac</platform></general></computer>"
-	output=`curl -sSkiu ${apiuser}:${apipass} "${jamfProURL}/${apiURL}" -H "Content-Type: text/xml" -d "${xmlHeader}${apiData}" -X POST`
+	output=`curl -sSkiu ${apiuser}:${apipass} "${jamfProURL}/${apiURL}" \
+	-H "Content-Type: text/xml" \
+	-d "${xmlHeader}${apiData}" \
+	-X POST`
 	#Error Checking
     	error=""
     	error=`echo $output | grep "Conflict"`
